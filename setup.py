369265ddff16fd7a42c5f252301b515910b638c7
Mikael Lindqvist <li.mikael@gmail.com>
2015-02-11 15:28:24 +0300

    broke out swagmaps to their own repo

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='swag',
-      version='1.0',
-      description='Tunapanda Swag Django app',
-      author='Tunapanda.Org',
-      author_email='brad@tunapanda.org',
-      packages=["swag"],
-     )
--- swag/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class SwagMap(models.Model):
-    # TODO: actual database backing
-    pass
